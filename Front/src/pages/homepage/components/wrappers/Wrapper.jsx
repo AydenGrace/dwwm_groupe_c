a193d4adfa928443890013d5e7b7e3a6cce8da3c
AydenGrace <pa.ce.hepl@gmail.com>
2024-06-27 08:45:06 +0200

    Jeudi

--- Front/src/pages/homepage/components/wrappers/Wrapper.jsx
+++ Front/src/pages/homepage/components/wrappers/Wrapper.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import CottageCard from "../../../../assets/components/cottageCard/CottageCard";
+import styles from "./Wrapper.module.scss";
 
 export default function Wrapper() {
   return (
     <section className="p-10">
       <h2 className="c-p">Section</h2>
-      <div className="w-100 d-flex flex-wrap justify-content-center">
+      <div className={`w-100 d-flex justify-content-center ${styles.Wrapper}`}>
         <CottageCard />
         <CottageCard />
         <CottageCard />
