3bbb064f90b4a379733506450f304571c077f916
Lenglet-jeremy <lengletjeremy.pro@gmail.com>
2024-06-28 11:52:37 +0200

    Mise à jours responsive Register/Login

--- Front/src/pages/login/Login.jsx
+++ Front/src/pages/login/Login.jsx
@@ -1,7 +1,60 @@
+import "./Login.css"
+
 export default function Login(){
     return(
-        <div>
-            Login
+        <div className="LoginPage">
+            
+            <div className="NavBar">
+                <p>NavBar</p>
+            </div>
+
+            <div className="LoginArea">
+                <div className="Form">
+                    <div className="Title">
+                        <h2>Connexion</h2>
+                    </div>
+                        <div className="AlignCenter">
+                            <div className="AlignLeft">
+
+                                <div className="UserID">
+
+
+                                    <div className="Email">
+                                        <p>Email : </p>
+                                        <input type="text" placeholder="Email"/>
+                                    </div>
+                                </div>
+
+                                <div className="Passwords">
+                                    <div className="Password">
+                                        <p>Mot de passe : </p>
+                                        <input type="password" placeholder="Mot de passe "/>
+                                    </div>
+
+                                </div>
+                            </div>
+                            <div className="AlreadyAccount">
+                                <p><a href="#">Vous n'avez pas de compte ? </a></p>
+                            </div>
+
+                            <div className="Submit">
+                                <button>Connexion</button>
+                            </div>
+
+                            <div className="Or">
+                                <p>OU</p>
+                            </div>
+
+                            <div className="TiersLogin">
+                                <a href="#">
+                                    <img src="LogoGoogle.png" alt="Logo Google" />
+                                    <p>Connexion avec Google</p>
+                                </a>
+                            </div>
+                            
+                        </div>
+                </div>
+            </div>
         </div> 
     )
 }
