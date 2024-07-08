66997f4e6419e5462af7ad32845a0aefe46a35e6
AydenGrace <pa.ce.hepl@gmail.com>
2024-07-08 10:40:05 +0200

    Error tests

--- Front/src/pages/ErrorPage.jsx
+++ Front/src/pages/ErrorPage.jsx
@@ -1,19 +1,24 @@
 import { useContext, useEffect } from "react";
 import styles from "./ErrorPage.module.scss";
 import { TitleContext } from "../contexts/TitleContext";
-import { Link } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
 
 export default function ErrorPage() {
   const { setTitle } = useContext(TitleContext);
+  const error = useRouteError();
+
+  console.log(error);
+
   useEffect(() => {
     setTitle("Erreur 404");
   }, []);
+
   return (
     <div className={`d-flex ${styles.page}`}>
       <section className={`d-flex flex-column w-100 ${styles.left}`}>
         <h1 className="mega">Oh non !</h1>
         <h2>Cette page est partie Co'gitée dans son coin !</h2>
-        <p>Code d'erreur - 404</p>
+        <p>Code d'erreur - {error ? error.status : 404}</p>
         <div className={`d-flex justify-content-center m-10`}>
           <Link className={`btn btn-primary`} to={"/"}>
             Revenir à l'accueil
--- Front/src/router.jsx
+++ Front/src/router.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
-import Account from "./pages/account/account.jsx";
+import Account from "./pages/account/Account.jsx";
 import ErrorPage from "./pages/ErrorPage.jsx";
 import HomePage from "./pages/homepage/HomePage.jsx";
 import Register from "./pages/register/Register";
@@ -16,43 +16,51 @@ export const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    // errorElement: <ErrorPage />,
     children: [
       {
         index: true,
         loader: HomepageLoader,
+        errorElement: <ErrorPage />,
         element: <HomePage />,
       },
       {
         path: "search",
+        errorElement: <ErrorPage />,
         element: <Search />,
       },
       {
         path: "concept",
+        errorElement: <ErrorPage />,
         element: <Concept />,
       },
       {
         path: "account",
+        errorElement: <ErrorPage />,
         element: <Account />,
       },
       {
         path: "register",
+        errorElement: <ErrorPage />,
         element: <Register />,
       },
       {
         path: "login",
+        errorElement: <ErrorPage />,
         element: <Login />,
       },
       {
         path: "profile",
+        errorElement: <ErrorPage />,
         element: <Profile />,
       },
       {
         path: "profile/ajoutgite",
+        errorElement: <ErrorPage />,
         element: <AjoutGite />,
       },
       {
         path: "details/:id",
+        errorElement: <ErrorPage />,
         element: <Details />,
       },
       {
